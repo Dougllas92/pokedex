bdc8b0bac1fb291dc62d73e000c3235e0b0ebb8c
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-05-11 23:08:15 -0300

    feat: fetchData and getPokemons

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 import {
   Wrapper,
   Gradient,
@@ -9,21 +10,84 @@ import {
   Title,
   Text,
   InputView,
-  InputSearch
+  InputSearch,
 } from './styles';
-
-import theme from '../../styles/theme';
 
 //import components
 import { SizedBox } from '../../components/SizedBox';
+import { Card } from '../../components/Card';
+import { Load } from '../../components/Load';
+
 
 //import svg's
 import Generation from '../../assets/icons/generation.svg';
 import Sort from '../../assets/icons/sort.svg';
 import Filter from '../../assets/icons/filter.svg';
-import Search from '../../assets/icons/search.svg';
+import api from '../../services/api';
+import { FlatList } from 'react-native';
+
+interface PokemonProps {
+  id: string;
+  name: string;
+  image: string;
+  types: string[];
+}
+
+interface DataProps {
+  name: string;
+  url: string;
+}
+
 
 export function Home() {
+  const [pokemons, setPokemons] = useState<PokemonProps[]>([])
+  const [nextPage, setNextPage] = useState<string>('pokemon-form?limit=10');
+  const [loading, setLoading] = useState(true);
+  const [loadingMore, setLoadingMore] = useState(false);
+
+
+  async function fetchData() {
+    setLoadingMore(true);
+
+    console.log(nextPage);
+    if (loadingMore)
+      return;
+
+    const { data } = await api.get(nextPage);
+    setNextPage(data.next);
+    await loadingPokemon(data.results);
+    setLoading(false);
+    setLoadingMore(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  async function loadingPokemon(data: Array<DataProps>) {
+    const pokemonData = await Promise.all(
+      data.map(async (pokemon) => {
+        const { data } = await api.get(pokemon.url);
+        const newUrl = pokemon.url.replace('https://pokeapi.co/api/v2/pokemon-form/', '').replace('/', '');
+
+        const image = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${newUrl}.svg`;
+
+        const pokemonCard = {
+          id: data.id,
+          name: data.name,
+          types: data.types.map((i: any) => i.type.name),
+          image,
+        }
+
+        return pokemonCard;
+      })
+    )
+    setPokemons([...pokemons, ...pokemonData]);
+  }
+
+  if (loading)
+    return <Load width={150} height={106} />
+
   return (
     <Wrapper>
       <Pokeball />
@@ -55,10 +119,25 @@ export function Home() {
                 inlineImagePadding={25}
               />
             </InputView>
-
           </Header>
         </Gradient>
       </GrandientPokeball>
-    </Wrapper>
+
+      <FlatList
+        data={pokemons}
+        style={{ paddingHorizontal: 20 }}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => {
+          return (
+            <Card data={item} />
+          )
+        }}
+        onEndReachedThreshold={0.1}
+        onEndReached={fetchData}
+        ListFooterComponent={
+          loadingMore ? <Load width={50} height={35} /> : <></>
+        }
+      />
+    </Wrapper >
   )
 }
--- src/screens/Home/styles.tsx
+++ src/screens/Home/styles.tsx
@@ -1,24 +1,23 @@
 import styled from 'styled-components/native';
 import LinearGradient from 'react-native-linear-gradient';
 
-import theme from '../../styles/theme';
+import { colors, gradient, typography } from '../../styles/theme';
+import PokeballSvg from '../../assets/patterns/pokeball-black.svg';
 
 
-import PokeballSvg from '../../assets/patterns/pokeball.svg';
-
-
-
+interface CardProps {
+  bgType: string;
+}
 
 export const Wrapper = styled.View`
   flex: 1;
   background-color: #FFF;
 `;
 
-
 export const Gradient = styled.View``;
 
 export const GrandientPokeball = styled(LinearGradient).attrs({
-  colors: theme.gradient.pokeball
+  colors: gradient.pokeball
 })``;
 
 export const Pokeball = styled(PokeballSvg).attrs({
@@ -29,7 +28,7 @@ export const Pokeball = styled(PokeballSvg).attrs({
 })``;
 
 export const Header = styled.View`
-  padding: 0px 40px;
+  padding: 0px 20px;
 `;
 
 export const Icons = styled.View`
@@ -38,17 +37,17 @@ export const Icons = styled.View`
 `;
 
 export const Title = styled.Text`
-  font-size: ${theme.typography.title.size};
-  line-height: ${theme.typography.title.line};
-  font-family: ${theme.typography.title.style};
-  color: ${theme.colors.text.black};
+  font-size: ${typography.title.size};
+  line-height: ${typography.title.line};
+  font-family: ${typography.title.style};
+  color: ${colors.text.black};
 `;
 
 export const Text = styled.Text`
-  font-size: ${theme.typography.description.size};
-  line-height: ${theme.typography.description.line};
-  font-family: ${theme.typography.description.style};
-  color: ${theme.colors.text.gray};
+  font-size: ${typography.description.size};
+  line-height: ${typography.description.line};
+  font-family: ${typography.description.style};
+  color: ${colors.text.gray};
 `;
 
 export const InputView = styled.View`
@@ -57,13 +56,65 @@ export const InputView = styled.View`
 
 export const InputSearch = styled.TextInput`
   width: 100%;
-  background-color: ${theme.colors.background.defaultInput};
+  background-color: ${colors.background.defaultInput};
   border-radius: 10px;
-  padding: 20px 25px;
-  font-size: ${theme.typography.description.size};
-  line-height: ${theme.typography.description.line};
-  font-family: ${theme.typography.description.style};
-  color: ${theme.colors.text.black};
+  padding: 20px 20px;
+  font-size: ${typography.description.size};
+  line-height: ${typography.description.line};
+  font-family: ${typography.description.style};
+  color: ${colors.text.black};
 `;
 
+export const CardPokemon = styled.View<CardProps>`
+  width: 100%;
+  border-radius: 10px;
+  background-color: ${props => colors.primary.[props.bgType].backgroundType};
+  padding: 20px;
 
+`;
+
+export const CardInfo = styled.View`
+  padding: 20px;
+`;
+
+export const CardNumber = styled.Text`
+  font-size: ${typography.pokemonNumber.size};
+  font-family: ${typography.pokemonNumber.style};
+  color: ${colors.text.black};
+`;
+
+export const CardTitle = styled.Text`
+  font-size: ${typography.name.size};
+  font-family: ${typography.name.style};
+  color: ${colors.text.white};
+`;
+
+export const Sprites = styled.View`
+  flex-direction: row;
+`
+
+export const SpritesCard = styled.View<CardProps>`
+  flex-direction: row;
+  background-color: ${props => colors.primary.[props.bgType].type};
+  padding: 5px;
+  margin-right: 5px;
+  border-radius: 3px;
+`;
+
+export const SpritesText = styled.Text`
+  font-size: ${typography.pokemonType.size};
+  font-family: ${typography.pokemonType.style};
+  color: ${colors.text.white};
+`;
+
+export const CardImg = styled.View`
+  position: absolute;
+  right: 10px;
+  bottom: 10px;
+`;
+
+export const CardViewPokeball = styled.View``;
+
+export const CardGrandientPokeball = styled(LinearGradient).attrs({
+  colors: gradient.pokeball
+})``;
