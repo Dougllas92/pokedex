8042237f849ce0969d9b7aaeef1cb0fabe5a70bd
Douglas Daniel Dutra Lopes <douglas.ddlopes@gmail.com>
2021-05-11 23:09:12 -0300

    style: new gradient for card

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -88,6 +88,27 @@ const colors = {
   }
 }
 
+const backgroundType = {
+  bug: '#8BD674',
+  dark: '#6F6E78',
+  dragon: '#7383B9',
+  electric: '#F2CB55',
+  fairy: '#EBA8C3',
+  fighting: '#EB4971',
+  fire: '#FFA756',
+  flying: '#83A2E3',
+  ghost: '#8571BE',
+  grass: '#8BBE8A',
+  ground: '#F78551',
+  ice: '#91D8DF',
+  normal: '#B5B9C4',
+  poison: '#9F6E97',
+  psychic: '#FF6568',
+  rock: '#D4C294',
+  steel: '#4C91B2',
+  water: '#58ABF6'
+}
+
 const typography = {
   title: {
     size: '32px',
@@ -105,8 +126,8 @@ const typography = {
     style: 'sf-pro-display-bold'
   },
   description: {
-    size: '16px',
-    line: '16px',
+    size: '14px',
+    line: '14px',
     style: 'sf-pro-display-regular'
   },
   pokemonNumber: {
@@ -122,7 +143,25 @@ const typography = {
 }
 
 const gradient = {
-  pokeball: ['rgba(245,245,245,0.97)', 'rgba(255,255,255,1)']
+  pokeball: ['rgba(245,245,245,0.97)', 'rgba(255,255,255,1)'],
+  bug: ['#8BD67499', '#8BD674'],
+  dark: ['#6F6E7899', '#6F6E78'],
+  dragon: ['#7383B999', '#7383B9'],
+  electric: ['#F2CB5599', '#F2CB55'],
+  fairy: ['#EBA8C399', '#EBA8C3'],
+  fighting: ['#EB497199', '#EB4971'],
+  fire: ['#FFA75699', '#FFA756'],
+  flying: ['#83A2E399', '#83A2E3'],
+  ghost: ['#8571BE99', '#8571BE'],
+  grass: ['#8BBE8A99', '#8BBE8A'],
+  ground: ['#F7855199', '#F78551'],
+  ice: ['#91D8DF99', '#91D8DF'],
+  normal: ['#B5B9C499', '#B5B9C4'],
+  poison: ['#9F6E9799', '#9F6E97'],
+  psychic: ['#FF656899', '#FF6568'],
+  rock: ['#D4C29499', '#D4C294'],
+  steel: ['#4C91B299', '#4C91B2'],
+  water: ['#58ABF699', '#58ABF6']
 }
 
-export default { colors, typography, gradient };
+export { colors, typography, gradient, backgroundType };
